51c709ef6ba56f351c55c3f05d9d8ee4fd2067f7
Habib Guliyev <graypit@gmail.com>
2021-04-30 17:31:43 +0400

    Added code

--- exercises/Day_3/task5/main.go
+++ exercises/Day_3/task5/main.go
@@ -8,23 +8,23 @@ import (
 
 func main() {
 	clearTerminal()
-	fmt.Print("Hi there! Please enter the 3 names of your co-workers!\n")
+	fmt.Print("Hi there! Please enter three names.\n")
 	getNames()
 }
 
 func getNames() {
-	fmt.Print("Enter the first person's name:")
+	fmt.Print("1.Enter the first person's name: ")
 	var first string
 	fmt.Scanln(&first)
-	fmt.Print("Enter the second person's name:")
+	fmt.Print("2.Enter the second person's name: ")
 	var second string
 	fmt.Scanln(&second)
-	fmt.Print("Enter the third person's name:")
+	fmt.Print("3.Enter the third person's name: ")
 	var third string
 	fmt.Scanln(&third)
 	clearTerminal()
 	fmt.Print("There are 3 people in classroom!")
-	fmt.Printf("\nHello %v!\nHi %v and %v!", first, second, third)
+	fmt.Printf("\nHello %v!\nHi %v and %v!\n", first, second, third)
 }
 
 func clearTerminal() {
